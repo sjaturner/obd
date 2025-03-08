dc011be87d5dbc84f66f6f9ff106cb9dbebfc24c
sjaturner <sjaturner@googlemail.com>
2025-03-08 18:24:37 +0000

    classifier for downshifts

--- process.m
+++ process.m
@@ -12,11 +12,11 @@ speed=retimed(:,3)/1.609344;
 throttle=retimed(:,4);
 gears=speed./rpm;
 plot(retimed(:,1),gears)
-pause
+%pause
 
 changes=gears-shift(gears,35);
 plot(retimed(:,1),changes);
-pause
+%pause
 
 function crossings = oneruns(input, threshold)
     rise_indices = find(diff(input) == 1) + 1;
@@ -33,13 +33,16 @@ end
 
 upshifts=changes > 0.0025;
 upshiftsat=oneruns(upshifts);
+upshiftsat=upshiftsat(upshiftsat(:,2)-upshiftsat(:,1)>25,:)
+
 downshifts=changes < -0.0025;
 downshiftsat=oneruns(downshifts);
+downshiftsat=downshiftsat(downshiftsat(:,2)-downshiftsat(:,1)>25,:)
 
 hold on
 plot([retimed(upshiftsat(:,1)),retimed(upshiftsat(:,2))],0.002.*[0 1],'r')
 plot([retimed(downshiftsat(:,1)),retimed(downshiftsat(:,2))],0.002.*[0 -1],'g')
-pause
+%pause
 
 ratios=floor(gears*10000);
 ratios(isnan(ratios))=1;
@@ -54,9 +57,27 @@ pkg load signal
 [pks, locs] = findpeaks(distribution, "MinPeakHeight",20,"MinPeakDistance",20)
 hold on
 plot(locs,pks,'ro')
-pause
-locs/=1000
+%pause
+locs/=10000
+gear_classifier_slop = 0.05
+gear_classifier=[locs * (1 - gear_classifier_slop), locs * (1 + gear_classifier_slop)]
 
 function plotter(out)
     clf;hold on;plot(out(:,1),out(:,2),'.b-');plot(out(:,1),out(:,3)/1.609344*100,'.r-');plot(out(:,1),out(:,4)/256*6000,'.g-')
+end
+
+downshiftsat=downshiftsat(:,1)-20;
+for index = 1:size(downshiftsat)
+    from = downshiftsat(index);
+    to = from+50
+    sub=retimed(from:to,:);
+    when=sub(1,1);
+    sub(:,1)-=when;
+    plotter(sub);
+    plot(0,0,'k.')
+    plot(0,6000,'k.')
+    find([gear_classifier(:,1) < gears(from) & gear_classifier(:,2) > gears(from)])
+    find([gear_classifier(:,1) < gears(to) & gear_classifier(:,2) > gears(to)])
+
+    pause
 end
